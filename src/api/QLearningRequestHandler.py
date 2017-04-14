6cfd54550da61f2db7b894274b81644d45d09ab7
tincho4t <martinforte@yahoo.com.ar>
2017-04-14 11:49:48 -0300

    Remove http server logs

--- src/api/QLearningRequestHandler.py
+++ src/api/QLearningRequestHandler.py
@@ -50,3 +50,7 @@ class QLearningRequestHandler(BaseHTTPRequestHandler):
         self.send_header('Access-Control-Allow-Methods', 'POST, PUT, OPTIONS')
         self.send_header("Access-Control-Expose-Headers", "Access-Control-Allow-Origin")
         self.send_header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+
+    def log_message(self, format, *args):
+        return
+
