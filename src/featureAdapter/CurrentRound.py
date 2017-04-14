d9734e83c6af98bbf46f8dbdd2a0897ed689f672
tincho4t <martinforte@yahoo.com.ar>
2017-04-14 14:49:22 -0300

    Normalize inputs

--- src/featureAdapter/CurrentRound.py
+++ src/featureAdapter/CurrentRound.py
@@ -1,6 +1,6 @@
-
+from __future__ import division
 
 class CurrentRound(object):
 	size = 1
 	def convert(self, requestDTO):
-		return [requestDTO.getCurrentRound()]
+		return [requestDTO.getCurrentRound() / 2]
--- src/featureAdapter/MyEnvidoScore.py
+++ src/featureAdapter/MyEnvidoScore.py
@@ -1,8 +1,8 @@
-
+from __future__ import division
 
 class MyEnvidoScore(object):
     size = 1
-    
+    MAX_ENVIDO_SCORE = 33
     def convert(self, requestDTO):
         points = self.getEnvidoScore(requestDTO.getInitialCards())
         return points
@@ -12,7 +12,7 @@ class MyEnvidoScore(object):
         possibleEnvidos.append(self.envidoFor(initialCards[0], initialCards[1]))
         possibleEnvidos.append(self.envidoFor(initialCards[0], initialCards[2]))
         possibleEnvidos.append(self.envidoFor(initialCards[1], initialCards[2]))
-        return [max(possibleEnvidos)]
+        return [max(possibleEnvidos)/ self.MAX_ENVIDO_SCORE]
 
     def envidoFor(self, cardOne, cardTwo):
         if(cardOne.suit == cardTwo.suit):
