08c36add27e9d48fd391603fc0a1afb0c11f1e43
tincho4t <martinforte@yahoo.com.ar>
2017-04-16 12:11:43 -0300

    Fix

--- src/featureAdapter/ScoreFeature.py
+++ src/featureAdapter/ScoreFeature.py
@@ -1,10 +1,10 @@
 from __future__ import division
 
-class ScoreFeature(FeatureAdapterInteface):
-	"""
-		Return your score and opponent score
-	"""
-	size = 2
-	def convert(self, requestDTO):
+class ScoreFeature(object):
+    """
+        Return your score and opponent score
+    """
+    size = 2
+    def convert(self, requestDTO):
         score = requestDTO.getScore()
         return [score.myScore / score.scoreToWin, score.opponentScore / score.scoreToWin]
