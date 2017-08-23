bc2212eab325016b9e83f0e61d2150eef2c7f1a5
Eitan <eitanrosenzvaig@gmail.com>
2017-08-23 16:56:39 -0300

    Adding adapters

--- src/player/QLearner.py
+++ src/player/QLearner.py
@@ -33,7 +33,7 @@ class QLearner(Player):
         super(QLearner, self).__init__()
         print "QLearner created!"
         self.dataFilePath = 'data.h5' # Where to save data for offline learning
-        self.adapters = [HandsWon()]
+        self.adapters = [SimplifyValueCard(), CardUsage(), CurrentRound(), IAmHand(), CountPossibleActions(), RivalCardsUsed(), EnvidoAdapter(), MyEnvidoScore(), ScoreFeature(), TrucoLevel(), PossibleActionsBitMap()]
         self.m = self.getFeatureSetSize() # Sum of all adapter sizes
         self.X = np.empty((0,self.m), int) # INPUT of NN (state of game before action)
         self.ACTION = np.array([]) # ACTION taken for input X
