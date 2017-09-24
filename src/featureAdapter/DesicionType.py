9b4df4558c77f8382819839f03b2589ee52a43a9
Eitan <eitanrosenzvaig@gmail.com>
2017-09-24 18:01:32 -0300

    Fixing wierd tabs

--- src/featureAdapter/DesicionType.py
+++ src/featureAdapter/DesicionType.py
@@ -1,19 +1,17 @@
-
-
 """
-	Define if the decision (Quiero/NoQuiero)
-	is related to Envido or Truco
-	[On/Off, Related to Envido, Related to Truco]
+    Define if the decision (Quiero/NoQuiero)
+    is related to Envido or Truco
+    [On/Off, Related to Envido, Related to Truco]
 
 """
 class DesicionType(object):
-	size = 3
+    size = 3
 
-	def convert(self, requestDTO):
+    def convert(self, requestDTO):
         desicionType = requestDTO.getDecisionType()
         if(desicionType == "Envido"):
-        	return [1,1,0]
+            return [1,1,0]
         elif(desicionType == "Truco"):
-        	return [1,0,1]
+            return [1,0,1]
         else:
-        	return [0,0,0]
+            return [0,0,0]
